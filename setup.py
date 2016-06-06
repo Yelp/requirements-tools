963fceaf38bc8291788e0c3f21ad38630830e67e
Chris Kuehl <ckuehl@yelp.com>
2016-06-06 15:58:37 -0700

    Release v0.5.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='check_requirements',
     description='Console script to help with testing requirements.',
-    version='0.4.0',
+    version='0.5.0',
     classifiers=[
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
