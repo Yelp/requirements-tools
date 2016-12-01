14c80c46f39ee85eb79123897eb4d2a20a7f6ca0
Anthony Sottile <asottile@umich.edu>
2016-12-01 10:36:30 -0800

    v0.7.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='check_requirements',
     description='Console script to help with testing requirements.',
-    version='0.7.5',
+    version='0.7.6',
     classifiers=[
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
