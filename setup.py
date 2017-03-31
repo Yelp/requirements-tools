d8e2f6d8e09aa12b00d1d838c95d454dc4d58d0d
Anthony Sottile <asottile@umich.edu>
2017-03-30 21:26:09 -0700

    v1.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(
     name='requirements-tools',
     description='Scripts for working with Python requirements.',
-    version='1.0.1',
+    version='1.1.0',
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
