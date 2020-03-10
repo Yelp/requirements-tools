fc2c26414b82f45fd5ff11a90d2580a15716a61c
Linlin Liu <linliu@yelp.com>
2020-03-09 17:03:31 -0700

    bump version to 1.2.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(
     name='requirements-tools',
     description='Scripts for working with Python requirements.',
-    version='1.2.2',
+    version='1.2.3',
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
